282d035929cadd55f7973dde14b5bbc3ae21f3fa
João Paulo <joao.pauloll@hotmail.com>
2022-09-03 15:45:47 -0300

    Add login form validators

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,24 +1,36 @@
-import { Input, Component, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Input, Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
-  form: FormGroup = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-  });
+  submitted = false;
+  
+  form!: FormGroup
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      username: new FormControl("", [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
 
   submit() {
-    if (this.form.valid) {
-      this.submitEM.emit(this.form.value);
-    }
+    this.submitted = true
+    console.log(this.form.valid);
+    // console.log(this.form.controls['username'].errors?.['required'])
   }
-  @Input() error: string | null | undefined;
 
-  @Output() submitEM = new EventEmitter();
+  get username() { return this.form.get('username')!; }
+  get password() { return this.form.get('password')!; }
+
 }
