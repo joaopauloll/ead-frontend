59cd2288696d314e8c0a23c9536f94ffa078f99a
João Paulo <joao.pauloll@hotmail.com>
2022-09-10 22:40:19 -0300

    Add home and profile pages

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user = this.authService.userValue.user
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
